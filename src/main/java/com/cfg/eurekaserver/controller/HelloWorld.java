111236e272da3966b64e19cea8cd8a7849cc1b65
chenfg <chenfg@yonyou.com>
2020-08-06 23:25:01 +0800

    添加hello测试方法

---
+++ src/main/java/com/cfg/eurekaserver/controller/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.cfg.eurekaserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 描述
+ * @author: chenfg
+ * @time: 2020/8/6 23:18
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("hello")
+    public Object helloworld(HttpServletRequest req){
+        String ip = req.getLocalAddr();
+        String port = String.valueOf(req.getLocalPort());
+        Map<String,Object> resultMap = new HashMap<>();
+        resultMap.put("ip",ip);
+        resultMap.put("port",port);
+        resultMap.put("message","hello I am "+ip+":"+port);
+        return resultMap;
+    }
+
+}
